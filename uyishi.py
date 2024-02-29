751e44705b0c55f770a1b121a2e3c8b7e45ff6d1
Salohiddin <nozimboevs@gmail.com>
2024-02-29 09:34:30 +0500

    9th commit

---
+++ uyishi.py
@@ -0,0 +1,201 @@
+# 1-misol
+# a = int(input())
+# if a>0:
+#    print(a+1)
+# if a<0: 
+#    print(a+0)
+
+
+
+# 2-misol
+# a = int(input()) 
+# if a>0:
+#     print(a+1)
+# if a<0:     
+#     print(a-2)
+
+
+# 3-misol
+# a = int(input())
+# if a>0:
+#     print(a+1)
+# if a==0:
+#     print(a+10)
+# else:
+#     print(a-2)
+
+
+
+
+# 4-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = 0 
+# if a>0 :
+#      d = d+1
+#      if b>0:
+#           d = d+1
+#           if c>0:
+#                d = d+1
+
+# print(str(d) + 'ta musbat son bor')
+
+
+
+# 5-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = 0 
+# e = 0
+# if a>0:
+#     d = d+1
+# if a<0:
+#     e = e+1
+# if b>0:
+#     d = d+1
+# if b<0:
+#     e = e+1
+# if c>0:
+#     d = d+1
+# if c<0:
+#     e = e+1
+# print(str(d) + 'ta musbat son bor') 
+# print(str(e) + 'ta manfiy son bor')
+
+
+# 6-misol
+# a = int(input())
+# b = int(input())
+# if a>b: 
+#    print(a)
+# if b>a:
+#    print(b)
+
+# 7-misol
+# a = int(input())
+# b = int(input())
+# if a<b: 
+#     print(a)
+#     if b<a:
+#         print(b)
+
+
+# 8-misol
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(b)
+#     if a<b:
+#         print(a)
+# print(a)
+# if b>a:
+#     print(b)
+
+# 9-misol
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(b)
+# if a>b:
+#     print(a)
+
+
+# 10-misol
+# a = int(input())
+# b = int(input())
+# if a==b:
+#     print(0)
+# else:
+#     c= a + b
+#     print(c)
+
+
+# 11-misol
+# a = int(input())
+# b = int(input())
+# if a==b:
+#     print(0)
+# if a>b:
+#     print(a)
+# if a<b: 
+#     print(b)
+
+
+# 12-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a>b:
+#     if b>c:
+#         print(c)
+#     else:
+#         print(b) 
+# else: 
+#     if b>a:
+#         if c>b:
+#             print(a)
+#         else:
+#             if a>c: 
+#                 print(c)
+#             else:
+#                 print(a)
+#     else:
+#         print(a)
+
+# 13-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a>b>c  or  c>b>a:
+#     k = b
+# elif b>a or a>c:
+#     k=a
+# else:
+#     k=c
+
+
+# 14-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# kichik = 0
+# kotta = 0
+# if a<b and a<c:
+#     kichik = a
+# else:
+#     if a>b and b<c:
+#         kichik = b 
+#     else:
+#         if a>c and b>c:
+#             kichik = c
+# if a>b and a>c:
+#      katta = a
+# else:
+#  if b>a and b>c:
+#      katta = b      
+#      if c>a and c>b:                                                  
+#          katta = c
+#          print(kichik)
+#          print(katta)
+
+
+# 15-misol
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a>b and b>c or c>b and b>a:
+#     if a>c:
+#         res = a + b 
+#     else:
+#         res = b + c 
+# else:
+#     if b>a and b>c:
+#         if a>c:
+#             res = b+a 
+#         else:
+#             res = b + c 
+#     else:
+#         res = a+c 
+# print(res)  
